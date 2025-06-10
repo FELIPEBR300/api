e21dd785276e5debb892d5966d8d792c80a0b2ca
FELIPE EDUARDO DE CAMARGO <felipe.ecamargo@senacsp.edu.br>
2025-06-10 20:20:08 -0300

    senac

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,50 @@
-const {Service: ServiceModel} = require("../models/Service");
+const { Service: ServiceModel } = require("../models/Service");
 
 const serviceController = {
+    create: async (req, res) => {
+        try {
+            const service = {
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
+            };
+
+            const response = await ServiceModel.create(service);
+
+            res.status(201).json({ response, msg: "Serviço criado com sucesso!" });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: "Erro ao criar o serviço." });
+        }
+    },
+
+    getAll: async (req, res) => {
+        try {
+            const services = await ServiceModel.find();
+            res.status(200).json(services);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: "Erro ao buscar os serviços." });
+        }
+    },
+
+    get: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const service = await ServiceModel.findById(id);
+
+            if (!service) {
+                return res.status(404).json({ msg: "Serviço não encontrado." });
+            }
+
+            res.status(200).json(service);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: "Erro ao buscar o serviço." });
+        }
+    }
 };
 
 module.exports = serviceController;
+;
--- routes/services.js
+++ routes/services.js
@@ -1,7 +1,13 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
-const serviceController = require("../controllers/serviceController")
+const serviceController = require("../controllers/serviceController");
+
+router.route("/services")
+    .post(serviceController.create);
 
 
+    router.route("/services").get((req, res) => serviceController.getAll(req, res));
+
+    router.route("/services/:id").get((req, res) => serviceController.getAll(req, res));
 
 module.exports = router;
